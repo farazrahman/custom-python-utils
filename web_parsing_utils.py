28cb635053b02245515fb12b44362c8ae5b47445
farazrahman <faraz.rahman41@gmail.com>
2021-05-29 20:22:11 +0530

    web parsing- get web response

---
+++ web_parsing_utils.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import smtplib
+import requests
+import json
+from bs4 import BeautifulSoup
+from typing import List, Dict
+from datetime import datetime, timedelta
+
+
+def fetch_website_details(url_address: str, header: Dict[str, str]):
+    """
+    :param url_address: get the url address of the website you want to parse
+    :param header: get the request headers. TODO: Add more details here
+    :return:
+    """
+    URL = url_address
+    header = header
+    page = requests.get(URL, headers=header)
+    page.raise_for_status()
+    soup = BeautifulSoup(page.text, 'html.parser')
+    response = soup.text
+    response_json = json.loads(response)
+    return response_json
